770903f5a757de766c119463fb365ffa7c7b07e3
francismario <francismariogomez@gmail.com>
2022-04-07 19:29:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - This is a description
+ * Return: 0 if successful, otherwise 1
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
