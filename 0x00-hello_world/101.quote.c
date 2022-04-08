b67919c42c0741cd9dd3483e899b3c39c5890e22
francismario <francismariogomez@gmail.com>
2022-04-07 20:15:31 -0700

    101.quote.c

---
+++ 0x00-hello_world/101.quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - This is a description
+ * Return: 0 if successful, otherwise 1
+ */
+int main(void)
+{
+	fprintf(stdout,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
