b99b91c3e889d26909a7b15c10f66f871c363534
francismario <francismariogomez@gmail.com>
2022-04-11 19:27:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - entry point for our program.
+ * Description: generates random number and sayes if it's +, -, or 0.
+ * Return: 1 if program exited on error, 0 if ended successfully.
+ */
+int main(void)
+{
+
+	char c = 'a';
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	return (0);
+}
