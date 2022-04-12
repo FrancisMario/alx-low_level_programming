d99759fbde12c3dfc314e54ea709665ed9b201d7
francismario <francismariogomez@gmail.com>
2022-04-11 17:59:12 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - entry point for our program.
+ * Description: generates random number and sayes if it's +, -, or 0.
+ * Return: 1 if program exited on error, 0 if ended successfully.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int last_int = n % 10;
+	char trailing_sentence[254];
+
+	if(last_int > 5){
+		strcpy(trailing_sentence, "and is greater than 5");
+	} else if (last_int == 0){
+		strcpy(trailing_sentence, "and is 0");
+	} else if(last_int < 6){
+		strcpy(trailing_sentence, "and is less then 6 and not 0");	
+	}
+	printf("Last digit of %d is %d %s \n", n, last_int, trailing_sentence);
+	return (0);
+}
