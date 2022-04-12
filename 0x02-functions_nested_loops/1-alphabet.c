5bc59adcdf571419b96df5e3ab052112754b984c
francismario <francismariogomez@gmail.com>
2022-04-12 16:37:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.c";
+#include "_putchar.c";
+
+/**
+ *
+ *
+ */
+
+void  print_alphabet(void) 
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; ++c)
+		_putchar(c);	
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
 /**
- *
+ * print_alphabet - prints the alphabet from a-z
+ * Description: prints the alphabet from a-z
+ * Return: void
  */
+
+void print_alphabet(void);
