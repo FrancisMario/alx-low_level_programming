525f07c38eab6b856f713e6ed22544125e94bf80
francismario <francismariogomez@gmail.com>
2022-04-12 16:17:29 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,10 +10,11 @@ int main(void)
 {
 	char value[] = "_putchar";
 	int i = 0;
-	
+	char c = 'x';
 	for (i = 0; value[i] != '\0'; ++i)
 	{
-		_putchar(*value[i]);
+		c = value[i];
+		_putchar(c);
 	}
 	_putchar("\n");
 	exit(0);
