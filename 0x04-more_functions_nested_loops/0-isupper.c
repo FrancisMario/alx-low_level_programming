a87091f97b66797b4784b867e4751031bedb9589
francismario <francismariogomez@gmail.com>
2022-04-14 16:34:09 -0700

    0-main.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -12,12 +12,12 @@ int lowercase_max = 122;
 int uppercase_min = 65;
 int uppercase_max = 90;
 
-if((c-lowercase_min) * (c-lowercase_max) <= 0)
+if ((c-lowercase_min) * (c-lowercase_max) <= 0)
 {
 	return 0;
 }
 
-if((c-uppercase_min) * (c-uppercase_max) <= 0)
+if ((c-uppercase_min) * (c-uppercase_max) <= 0)
 {
 	return 1;
 }
