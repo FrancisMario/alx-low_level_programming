a511977896a629426991dd7f36efb5e33b1c8445
francismario <francismariogomez@gmail.com>
2022-04-14 10:31:57 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,48 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int  _isupper(char);
+int  _isupper(char c);
+int  _isdigit(char c);
+
+
+/**
+ * _isupper - checks if a character is upper or lower case
+ *
+ * Returns: 0 if lowercase 1 if uppercase
+ */
+int _isupper(int c)
+{
+        int lowercase_min = 97;
+        int lowercase_max = 122;
+        int uppercase_min = 65;
+        int uppercase_max = 90;
+
+        if((c-lowercase_min) * (c-lowercase_max) <= 0)
+        {
+                return 0;
+        }
+
+        if((c-uppercase_min) * (c-uppercase_max) <= 0)
+        {
+                return 1;
+        }
+
+}
+
+/**
+ * _isdigit - checks if a character is upper or lower case
+ *
+ * Returns: 0 if lowercase 1 if uppercase
+ */
+int _isdigit(int c)
+{
+        int digit_min = 97;
+        int digit_max = 122;
+        if((c-digit_min) * (c-digit_max) <= 0)
+        {
+                return 1;
+        }
+	return 0;
+}
+
+#endif
