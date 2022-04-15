254d09f6037441d36125bc645975c74a8f8a64f4
francismario <francismariogomez@gmail.com>
2022-04-14 18:01:45 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two digits.
+ * @a: parameter a
+ * @b: parameter b
+ * Return: 0 if lowercase 1 if uppercase
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
