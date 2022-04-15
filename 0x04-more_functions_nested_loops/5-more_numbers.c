7cf0c879d8a632efe61297542821c8bb02806560
francismario <francismariogomez@gmail.com>
2022-04-14 18:39:26 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers.
+ * Description: prints numbers from 9 - 0
+ */
+void print_numbers(void)
+{
+int i;
+
+i = 0;
+
+for (i = 0; i <= 10; i++)
+{
+	for (i = 0; i <= 14; i++)
+	{
+	_putchar(i + '0');
+	}
+_putchar('\n');
+}
+_putchar('\n');
+}
