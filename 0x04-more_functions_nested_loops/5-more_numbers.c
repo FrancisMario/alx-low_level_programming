f5de6abeeb52cfcffe78fb1c0ab98742c4c79ef2
francismario <francismariogomez@gmail.com>
2022-04-14 18:44:11 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,10 @@ i = 0;
 for (i = 0; i <= 10; i++)
 {
 j = 0;
-	for (j = 0; j <= 14; j++)
-	{
-	_putchar(i + '0');
-	}
+for (j = 0; j <= 14; j++)
+{
+_putchar(i + '0');
+}
 _putchar('\n');
 }
 _putchar('\n');
