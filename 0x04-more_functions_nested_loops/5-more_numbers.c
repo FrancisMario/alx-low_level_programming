22d6283ff25c014963f0d56d7090b5cebaf018a9
francismario <francismariogomez@gmail.com>
2022-04-14 19:03:16 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,16 @@ void more_numbers(void)
 {
 int i;
 int j;
+
 i = 0;
 j = 0;
+
 for (i = 1; i <= 10; i++)
 {
-for (j = 0; j <= 14; j++)
-{
-_putchar(j + '0');
-}
+	for (j = 0; j <= 14; j++)
+	{
+	_putchar(j + '0');
+	}
 _putchar('\n');
 }
 _putchar('\n');
