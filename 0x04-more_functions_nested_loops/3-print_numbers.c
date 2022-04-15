a962c1f30133a2e40764a8b8029f2235e5ea2706
francismario <francismariogomez@gmail.com>
2022-04-14 18:11:40 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * mul - multiplies two digits.
+ * Description: prints numbers from 9 - 0
+ */
+void print_numbers(void)
+{
+	printLol(9);
+}
+
+void printLol(int v)
+{
+	if (!v > 0)
+	{
+		_printchar(v);
+		printLol(v--);
+	}
+	else
+	{
+	  _printchar('\n');
+  }
+}
