434c2d18fa6dc588fb69e123d87f4b8c6173e4b2
francismario <francismariogomez@gmail.com>
2022-04-20 16:16:48 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int- swaps the value of two given ints
+ * @a: mem address of first int 
+ * @b: mem address of second int
+ */
+void swap_int(int *a, int *b)
+{
+	int *temp_a = &a;
+	*a = &b;
+	*b = &temp_a;
+	
+}
