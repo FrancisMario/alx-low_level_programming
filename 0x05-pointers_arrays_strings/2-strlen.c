c67a36b617a0b9b929b3946b2925b9b2540c3b8f
francismario <francismariogomez@gmail.com>
2022-04-20 16:38:37 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @a: mem address of first int
+ * @b: mem address of second int
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s != '\0') 
+	{
+		counter++;
+		*s++;
+	}
+	return counter;
+}
