1b59b41249ffdab59ec02e5f4451851b7596ef1a
francismario <francismariogomez@gmail.com>
2022-04-20 17:05:59 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_rev - prints a string in reverse
+ * @str: parameter
+ */
+void print_rev(char *s)
+{
+	while (*s != "\0")
+	{
+		_putchar(s);
+	}
+}
